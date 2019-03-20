7514afaf0f63318aac8198743a51dd87933d7cc9
David Grossman <2gotgrossman@gmail.com>
2019-03-20 13:05:26 -0400

    init commit of working flask server

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import 
+import os
+
+import data
+
+HAZARD_TYPES = ['volcanoes', 'earthquakes']
+SUPPORTED_IMAGE_TYPES = ["geo_backscatter", "geo_coherence",
+               "geo_interferogram", "ortho_backscatter",
+               "ortho_coherence", "ortho_interferogram"]
+
+SUPPORTED_SATELLITES = []
+
+app = Flask(__name__)
+
+@app.route('/api/<string:hazard_type>', methods=['GET'])
+def get_hazards_summary_info(hazard_type):
+    if hazard_type == 'volcanoes':
+        return jsonify(data.get_all_volcano_summary_info())
+    else:
+        abort(404, "Hazard Type {0} does not exist.".format(hazard_type))
+
+@app.route('/api/<string:hazard_type>/<string:hazard_id>', methods=['GET'])
+def get_hazard_info_page(hazard_type, hazard_id):
+    # Check if hazard_type is legit
+    if hazard_type not in HAZARD_TYPES:
+        abort(404, "Hazard Type {0} does not exist.".format(hazard_type))
+    elif hazard_type == 'earthquakes':
+        abort(500, "The earthquake Hazard Type is currently not supported")
+    elif hazard_type == 'volcanoes':
+        if hazard_id != data.HAZARD_INFO_TEMPLATE['hazard_id']:
+            abort("The hazard with id {0} does not exist as a {1} Hazard Type.".format(hazard_id, hazard_type))
+        else:
+            image_types_requested = request.args.get('image_types')
+
+            types = set()
+            if image_types_requested is not None:
+                image_types_requested = image_types_requested.split(",")
+                for image_type in image_types_requested:
+                    if image_type in SUPPORTED_IMAGE_TYPES:
+                        types.add(image_type)
+                if len(types) == 0:
+                    abort(400, "None of the following image types are supported: '{0}'".format(str(image_types_requested)))
+
+            # Check that these variables are of the correct type
+            satellites_requested = request.args.get('satellites')
+            start_date, end_date = request.args.get('start_date'), request.args.get('end_date')
+
+            satellites = set()
+            if satellites_requested is not None:
+                satellites_requested = satellites_requested.split(',')
+                for satellite in satellites_requested:
+                    if satellite in SUPPORTED_SATELLITES:
+                        satellites.add(satellite)
+                if len(satellites) == 0:
+                    abort(400, "None of the following satellites are supported: '{0}'".format(str(satellites_requested)))
+
+            if start_date is not None:
+                if not len(start_date) == 8 or not start_date.isdigit():
+                    abort(400, "Incorrectly formatted start_date: {0}".format(start_date))
+            if end_date is not None:
+                if not len(end_date) == 8 or not end_date.isdigit():
+                    abort(400, "Incorrectly formatted end_date: {0}".format(end_date))
+
+            max_num_images = request.args.get('max_num_images')
+            if max_num_images is None:
+                max_num_images = 0
+            elif not max_num_images.isdigit():
+                abort(400, "Parameter 'max_num_images' must be an integer")
+            else:
+                max_num_images = int(max_num_images)
+
+
+            # Build request
+            return jsonify(data.get_volcano_hazard_info_by_id(hazard_id, types, satellites, max_num_images))
+
+@app.route('/api/images/<string:image_id>')
+def get_image(image_id):
+    print("YO ITS ME", os.path.realpath(__file__))
+    location = data.get_image_file_location(image_id)
+    print("AFTER YO IT'S ME. location: ", location, type(location))
+
+    if location is None:
+        abort(404, "Image with image id {0} does not exist.".format(str(image_id)))
+    else:
+        file_loc = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data_files')
+        print("FILE LOCATION", file_loc)
+        return send_file(os.path.join(file_loc, location), as_attachment=True, mimetype='image/jpeg')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': str(error)}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': str(error)}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,105 @@
+import copy
+
+URL_BASE = 'localhost:5000'
+DATE_FOR_IMAGES = '03061999'
+COMPRESSED_IMAGE_URL = '/api/images/scatter03061999_compressed.jpg'
+FULL_IMAGE_URL = '/api/images/scatter03061999_full.jpg'
+COMPRESSED_IMAGE_FILE_LOCATION = 'P61i5ZR_compressed.jpg'
+FULL_IMAGE_FILE_LOCATION = 'P61i5ZR_full.jpg'
+
+DEFAULT_NUM_IMAGES = 5
+
+LAT_LONGS = [{'latitude': 0.0, 'longitude': 0.0} for i in range(1)]
+
+VOLCANO_HAZARD_TEMPLATES = {
+    'type': 'volcanoes',
+    'hazards': list()
+}
+
+HAZARD_INFO_TEMPLATE = {
+    'hazard_id': 'VolcanoName',
+    'name': 'Volcano Name',
+    'location': {
+        'latitude': 0.0,
+        'longitude': 0.0
+    },
+    'last_updated': '05182019'
+}
+
+SUPPORTED_IMAGE_TYPES = ["geo_backscatter", "geo_coherence",
+               "geo_interferogram", "ortho_backscatter",
+               "ortho_coherence", "ortho_interferogram"]
+
+def IMAGE_TYPE_DATA_TEMPLATE(num_images):
+    return [
+        {
+            'date': DATE_FOR_IMAGES,
+            'compressed_image_url': COMPRESSED_IMAGE_URL,
+            'full_image_url': FULL_IMAGE_URL
+        }
+        for _ in range(num_images)
+    ]
+
+def HAZARD_DATA_TEMPLATE(num_images):
+    return {
+    'hazard_id': 'VolcanoName',
+    'hazard_name': 'Volcano Name',
+    'last_updated': '05182019',
+    'location': {
+        'latitude': 0.0,
+        'longitude': 0.0
+      },
+    'images_by_satellite': {
+        'satellite_id1': {image_type: IMAGE_TYPE_DATA_TEMPLATE(num_images) for image_type in SUPPORTED_IMAGE_TYPES},
+        'satellite_id2': {image_type: IMAGE_TYPE_DATA_TEMPLATE(num_images) for image_type in SUPPORTED_IMAGE_TYPES},
+    }
+}
+
+
+
+def get_all_volcano_summary_info():
+    '''
+    Generates and returns the data needed for the landing page endpoint(URL: /api/:hazard_type).
+
+    This function injects the number of volcanoes returned and the lat-long info into the VOLCANO_HAZARD_TEMPLATES.
+    :return: dict
+    '''
+
+    response = copy.deepcopy(VOLCANO_HAZARD_TEMPLATES)
+
+    for latitude_longitude in LAT_LONGS:
+        current_hazard = copy.deepcopy(HAZARD_INFO_TEMPLATE)
+        current_hazard['location'] = latitude_longitude
+        response['hazards'].append(current_hazard)
+
+    return response
+
+def get_volcano_hazard_info_by_id(hazard_id, image_types, satellites, max_num_images):
+    response = copy.deepcopy(HAZARD_DATA_TEMPLATE(
+        num_images=max_num_images if max_num_images is not 0 else DEFAULT_NUM_IMAGES))
+    if len(satellites) != 0:
+        for satellite in HAZARD_DATA_TEMPLATE['images_by_satellite']:
+            # TODO: TAKE CARE OF `satellites` being None case
+            if satellites is not None and satellite not in satellites:
+                del response['images_by_satellite'][satellite]
+            else:
+                if len(image_types) != 0:
+                    for image_type in SUPPORTED_IMAGE_TYPES:
+                        # TODO: TAKE CARE OF `image_types` being None case
+                        if image_types is not None and image_type not in image_types:
+                            del response['images_by_satellite'][satellite][image_type]
+                        else:
+                            if max_num_images > 0:
+                                response['images_by_satellite'][satellite][image_type] = \
+                                    response['images_by_satellite'][satellite][image_type][:max_num_images]
+    return response
+
+
+def get_image_file_location(image_id):
+    print("FULL URL:", FULL_IMAGE_URL.split('/')[-1])
+    if image_id == FULL_IMAGE_URL.split('/')[-1]:
+        return FULL_IMAGE_FILE_LOCATION
+    elif image_id == COMPRESSED_IMAGE_URL.split('/')[-1]:
+        return COMPRESSED_IMAGE_FILE_LOCATION
+    else:
+        return None
